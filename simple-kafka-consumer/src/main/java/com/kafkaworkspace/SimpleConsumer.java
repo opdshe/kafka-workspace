52b1f65d315c6c214b0e096797160b9647c9344b
opdshe <opdshe@naver.com>
2020-07-23 14:53:36 +0900

    Add : simple-kafka-consumer 추가

---
+++ simple-kafka-consumer/src/main/java/com/kafkaworkspace/SimpleConsumer.java
@@ -0,0 +1,36 @@
+package com.kafkaworkspace;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class SimpleConsumer {
+    private static final String TOPIC_NAME = "test";
+    private static final String GROUP_ID = "testgroup";
+    private static final String BOOTSTRAP_SERVERS = "13.124.221.140:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
+
+        consumer.subscribe(Collections.singleton(TOPIC_NAME));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record.value());
+            }
+        }
+    }
+}
